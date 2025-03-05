023ebef31b1fcf7aa44b45a78ec0cdfef96f6830
davidmicah123 <micahdanielsd@gmail.com>
2025-03-05 14:28:14 +0100

    Updated website

---
+++ src/Components/Homepagehero/Homepagehero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from "./Homepagehero.module.css"
+
+const Homepagehero = () => {
+  return (
+    <div>Homepagehero</div>
+  )
+}
+
+export default Homepagehero;
