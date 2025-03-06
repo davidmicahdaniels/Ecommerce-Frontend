2ff76d319e965e12026e3c19dd94ee15197c8472
davidmicah123 <micahdanielsd@gmail.com>
2025-03-06 14:33:37 +0100

    Updated website

--- src/Components/Homepagehero/Homepagehero.js
+++ src/Components/Homepagehero/Homepagehero.js
@@ -5,6 +5,11 @@ const Homepagehero = () => {
   return (
     <section className={classes.homepage_hero}>
       <h1>CONNECT, BUY, AND SELL WITHIN ABIOLA AJIMOBI TECHNICAL UNIVERSITY</h1>
+
+      <div className={classes.hero_buttons_wrapper}>
+        <button>Buy Now</button>
+        <button>Become a Seller</button>
+      </div>
     </section>
   );
 }
