9635a1e5c249d08461ec1b93c260435d33931f99
davidmicah123 <micahdanielsd@gmail.com>
2025-03-06 20:01:25 +0100

    Updated website

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,23 +4,25 @@ import classes from "./Navbar.module.css"
 const Navbar = () => {
   return (
     <nav>
-      <h1>Logo</h1>
+      <div className={classes.nav_content_wrapper}>
+        <h1>Logo</h1>
 
-      <ul className={classes.nav_links_wrapper}>
-        <li>Home</li>
-        <li>Category</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-      </ul>
+        <ul className={classes.nav_links_wrapper}>
+          <li>Home</li>
+          <li>Category</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+        </ul>
 
-      <div className={classes.searchbar_wrapper}>
-        icon
-        <input type="text" />
-      </div>
+        <div className={classes.searchbar_wrapper}>
+          icon
+          <input type="text" />
+        </div>
 
-      <div className={classes.nav_buttons_wrapper}>
-        <button>Sign up</button>
-        <button>Sign in</button>
+        <div className={classes.nav_buttons_wrapper}>
+          <button>Sign up</button>
+          <button>Sign in</button>
+        </div>
       </div>
     </nav>
   );
