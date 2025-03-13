b3a000059794201ce35bf2cb7e7a2c3a76afbd21
davidmicah123 <micahdanielsd@gmail.com>
2025-03-13 12:00:55 +0100

    Updated website

--- src/Components/DashboardNavbar/DashboardNavbar.js
+++ src/Components/DashboardNavbar/DashboardNavbar.js
@@ -3,8 +3,16 @@ import classes from "./DashboardNavbar.module.css"
 
 const DashboardNavbar = () => {
   return (
-    <nav className={classes.dashboard_nav}>DashboardNavbar</nav>
-  )
+    <nav className={classes.dashboard_nav}>
+      <div className={classes.dashboard_nav_wrapper}>
+        <div>
+            <ion-icon name="cart-outline"></ion-icon>
+            <h2>EduMart</h2>
+        </div>
+        <ion-icon name="menu-outline"></ion-icon>
+      </div>
+    </nav>
+  );
 }
 
 export default DashboardNavbar;
