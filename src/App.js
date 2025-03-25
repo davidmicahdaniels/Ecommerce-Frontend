ea473ba1bf24d283b2a75da73104d1e6e2b9c849
davidmicah123 <micahdanielsd@gmail.com>
2025-03-25 14:02:13 +0100

    Created vendor login and signup pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./Pages/Home/Home";
 import UserDashboard from "./Pages/UserDashboard/UserDashboard";
 import UserLogin from "./Pages/UserLogin/UserLogin";
 import UserSignup from "./Pages/UserSignup/UserSignup";
+import Login from "./Components/Vendor/Login/Login";
+import Signup from "./Components/Vendor/Signup/Signup";
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/usersignup" element={<UserSignup />} />
           <Route exact path="/userlogin" element={<UserLogin />} />
+          <Route exact path="/vendorlogin" element={<Login />} />
+          <Route exact path="/vendorsignup" element={<Signup />} />
           <Route exact path="/shop" element={<UserDashboard />} />
         </Routes>
       </BrowserRouter>
---
+++ src/Components/Vendor/Login/Login.js
@@ -0,0 +1,52 @@
+import React from "react";
+import classes from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+import img from "../../../Assets/Images/signup.jpg";
+import Navbar from "../../../Components/Navbar/Navbar";
+import Footer from "../../../Components/Footer/Footer";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSignup = () => {
+    navigate("/vendorsignup");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <section className={classes.signup_form_section}>
+        <div className={`${classes.from_content_wrapper} ${classes.alinged}`}>
+          <div className={classes.signup_form_img_wrapper}>
+            <img src={img} alt="" className={classes.form_img} />
+          </div>
+          <div className={classes.form_action_box}>
+            <h2>Vendor Login</h2>
+
+            <div className={classes.input_box}>
+              <p>Email:</p>
+              <input type="text" placeholder="Enter your email" />
+            </div>
+
+            <div className={classes.input_box}>
+              <p>Password:</p>
+              <input type="text" placeholder="Enter your password" />
+            </div>
+
+            <button>Login</button>
+
+            <small className={classes.login_action}>
+              Don't have an account yet?{" "}
+              <a href="#" onClick={handleSignup}>
+                Signup
+              </a>
+            </small>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Login;
