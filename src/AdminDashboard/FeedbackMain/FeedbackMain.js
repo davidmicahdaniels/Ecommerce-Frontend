f18cf3766cbc61b50edb5355b6cc42794cb3fe9c
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-04-29 00:30:01 +0100

    Updated feedback review

--- src/AdminDashboard/FeedbackMain/FeedbackMain.js
+++ src/AdminDashboard/FeedbackMain/FeedbackMain.js
@@ -1,7 +1,6 @@
-import classes from "./FeedbackMain.module.css"
-
-
 import React from 'react'
+import classes from "./FeedbackMain.module.css"
+import { FaStar } from "react-icons/fa";
 
 const FeedbackMain = () => {
   return (
@@ -10,7 +9,18 @@ const FeedbackMain = () => {
 
 
       <div className={classes.feesbacks_box_wrapper}>
-        <div className={classes.feedback_box}></div>
+        <div className={classes.feedback_box}>
+          <h2>Bad product condition</h2> 
+          <p>The goods i ordered was delivered to me in avery bad condition rather than what i saw on the website...</p>
+          <div className="">
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+            <FaStar />
+          </div>
+          <button className="">View Review</button>
+        </div>
       </div>
     </div>
   )
