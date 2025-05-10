cf1c4eb2f7431d89b9ba44a14804231b3d630adf
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-10 16:56:16 +0100

    Updating vendors profile

--- src/VendorsDashboard/VendorsProfile/VendorProfile.js
+++ src/VendorsDashboard/VendorsProfile/VendorProfile.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import classes from "./VendorProfile.module.css"
+import prod from "../../Assets/Images/prod3.png"
+import { FaStar } from "react-icons/fa";
 
 const VendorProfile = () => {
   return (
     <div className={classes.vendors_profile}>
       <div className={classes.profile_main}>
         <h2>Vendor's Profile</h2>
+
+
+
       </div>
       <div className={classes.vendor_product_display}>
         <h3>Your Products</h3>
+
+        <div className={classes.profile_product_list}>
+          <div className={classes.product_card}>
+            <div className={classes.prod_img_wrapper}>
+                <img src={prod} alt="" />
+            </div>
+            <div>
+              <h3>Crochet Hooks</h3>
+              <p><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+              <p>5 star of 30 reviews</p>
+              <button>Manage Product</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
