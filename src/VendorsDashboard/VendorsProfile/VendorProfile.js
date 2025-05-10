bac5ce6bf53f600c7dc2540fbe41b6c3788fa3d1
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-10 17:33:03 +0100

    Updating vendor's profile

--- src/VendorsDashboard/VendorsProfile/VendorProfile.js
+++ src/VendorsDashboard/VendorsProfile/VendorProfile.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import classes from "./VendorProfile.module.css"
 import prod from "../../Assets/Images/prod3.png"
+import prod1 from "../../Assets/Images/prod4.png"
+import prod2 from "../../Assets/Images/prod2.png"
 import { FaStar } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 const VendorProfile = () => {
   return (
     <div className={classes.vendors_profile}>
       <div className={classes.profile_main}>
-        <h2>Vendor's Profile</h2>
+        <div className={classes.profile_top}>
+          <h2>Vendor's Profile</h2>
+          <button>Edit Profile</button>
+        </div>
+
+        <div>
+          <div className={classes.brand_essentials}>
+            <div className={classes.brand_img_Wrapper}>
+              <img src={prod} alt='brand logo' />
+            </div>
+            <div>
+              <h2>Premium Class Store</h2>
+              <p><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+              <small>5 star of 45 reviews</small>
+              <p className={classes.location}><FaLocationDot /> <p>Abuja, Nigeria</p></p>
+            </div>
+          </div>
+          <p>Description:</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+
+          <p>Address:</p>
+          <p>Plot 7 Jakande street, afebabalola avenue, Abuja</p>
+        </div>
 
 
 
@@ -20,13 +46,39 @@ const VendorProfile = () => {
             <div className={classes.prod_img_wrapper}>
                 <img src={prod} alt="" />
             </div>
-            <div>
-              <h3>Crochet Hooks</h3>
+            <div className={classes.card_content}>
+              <h4>Crochet Hooks</h4>
               <p><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
-              <p>5 star of 30 reviews</p>
+              <small>5 star of 30 reviews</small>
               <button>Manage Product</button>
             </div>
           </div>
+          <div className={classes.product_card}>
+            <div className={classes.prod_img_wrapper}>
+                <img src={prod2} alt="" />
+            </div>
+            <div className={classes.card_content}>
+              <h4>Crochet Hooks</h4>
+              <p><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+              <small>5 star of 30 reviews</small>
+              <button>Manage Product</button>
+            </div>
+          </div>
+          <div className={classes.product_card}>
+            <div className={classes.prod_img_wrapper}>
+                <img src={prod} alt="" />
+            </div>
+            <div className={classes.card_content}>
+              <h4>Crochet Hooks</h4>
+              <p><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></p>
+              <small>5 star of 30 reviews</small>
+              <button>Manage Product</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={classes.action_btn_wrapper}>
+          <button>View All Products</button>
         </div>
       </div>
     </div>
