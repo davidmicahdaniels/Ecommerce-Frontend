32ce0de57352eac38da4a150bf578410a88dbec3
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-26 21:30:52 +0100

    Updated navigation

--- src/Components/DashboardNavbar/DashboardNavbar.js
+++ src/Components/DashboardNavbar/DashboardNavbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import classes from "./DashboardNavbar.module.css"
 import { BsCartPlusFill    } from "react-icons/bs";
+import { useNavigate } from 'react-router-dom';
 
 const DashboardNavbar = () => {
+  const navigate = useNavigate();
+
+  const visitLandingPage = () => {
+    navigate("/")
+  }
+
   return (
     <nav className={classes.dashboard_nav}>
       <div className={classes.dashboard_nav_wrapper}>
-        <div className={classes.logo}>
+        <div className={classes.logo} onClick={visitLandingPage}>
             <ion-icon name="cart-outline"></ion-icon>
             <h2>EduMart</h2>
         </div>
