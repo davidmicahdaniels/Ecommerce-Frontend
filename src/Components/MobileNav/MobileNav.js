372683a4f4b6abebcb899ba9a692622a9a165765
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-27 16:05:57 +0100

    Updated styling and routing

--- src/Components/MobileNav/MobileNav.js
+++ src/Components/MobileNav/MobileNav.js
@@ -1,11 +1,29 @@
 import React from "react";
 import classes from "./MobileNav.module.css";
+import { useNavigate } from "react-router-dom";
 
 const MobileNav = (props) => {
+  const navigate =  useNavigate();
+
+  const visitHome = () => {
+    navigate("/");
+  }
+  const visitLogin = () => {
+    navigate("/userlogin");
+  }
+
+  const visitSignup = () => {
+    navigate("/usersignup");
+  }
+
+  const visitVendorSignup = () => {
+    navigate("/vendorsignup");
+  }
+
   return (
     <nav className={classes.mobilenav}>
       <div className={classes.nav_content_wrappe}>
-        <h1>EduMart</h1>
+        <h1 onClick={visitHome} className={classes.edumart_mobilenav_logo}>EduMart</h1>
 
         <ul className={classes.nav_links_wrapper}>
           <li>Home</li>
@@ -15,12 +33,12 @@ const MobileNav = (props) => {
         </ul>
 
         <div className={classes.nav_buttons_wrapper}>
-          <button className={classes.colore_btn}>Sign up</button>
-          <button className={classes.btn}>Sign in</button>
+          <button className={classes.colore_btn} onClick={visitSignup}>Sign up</button>
+          <button className={classes.btn} onClick={visitLogin}>Sign in</button>
         </div>
 
         <div className={classes.seller_btn_wrapper}>
-          <button className={classes.btn}>
+          <button className={classes.btn} onClick={visitVendorSignup}>
             <p>Become a Seller</p>
             <ion-icon name="arrow-forward-outline"></ion-icon>
           </button>
