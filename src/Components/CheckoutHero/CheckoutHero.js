f627cf774285490a3a25e904708c39054f3513fb
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-28 19:40:00 +0100

    Developing checkout page

---
+++ src/Components/CheckoutHero/CheckoutHero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./CheckoutHero.module.css"
+
+const CheckoutHero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CheckoutHero;
---
+++ src/Pages/OrderCheckout/OrderCheckout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardNavbar from '../../Components/DashboardNavbar/DashboardNavbar';
+
+const OrderCheckout = () => {
+  return (
+    <div>
+      <DashboardNavbar />
+      
+    </div>
+  )
+}
+
+export default OrderCheckout;
