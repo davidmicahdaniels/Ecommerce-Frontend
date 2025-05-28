ef7b31bfbcd27c567b884ee1bf34a02a2bf76fd9
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-28 20:28:02 +0100

    Working on checkout page

--- src/Components/CheckoutBody/CheckoutBody.js
+++ src/Components/CheckoutBody/CheckoutBody.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from "./CheckoutBody.module.css"
 import { useNavigate } from 'react-router-dom';
+import img from "../../Assets/Images/prod3.png"
 
 const CheckoutBody = () => {
     const navigate =  useNavigate();
@@ -11,7 +12,62 @@ const CheckoutBody = () => {
   return (
     <div className={classes.checkout_body}>
       <div className={classes.checkout_content_wrapper}>
-        <h2>Checkout Items</h2>
+
+        <div>
+            <h2>Checkout Items</h2>
+
+
+            <div className={classes.items_list}>
+                <div className={classes.item}>
+                    <div className={classes.item_img_wrapper}>
+                        <img src={img} alt="cart item" />
+                    </div>
+                    <div className={classes.item_text_wrapper}>
+                        <h3>2 ball of crochet wools</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt et dolore magna aliqua....</p>
+                    </div>
+                    <div className={classes.action_area}>
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                </div>
+                <div className={classes.item}>
+                    <div className={classes.item_img_wrapper}>
+                        <img src={img} alt="cart item" />
+                    </div>
+                    <div className={classes.item_text_wrapper}>
+                        <h3>2 ball of crochet wools</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt et dolore magna aliqua....</p>
+                    </div>
+                    <div className={classes.action_area}>
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                </div>
+                <div className={classes.item}>
+                    <div className={classes.item_img_wrapper}>
+                        <img src={img} alt="cart item" />
+                    </div>
+                    <div className={classes.item_text_wrapper}>
+                        <h3>2 ball of crochet wools</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt et dolore magna aliqua....</p>
+                    </div>
+                    <div className={classes.action_area}>
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                </div>
+                <div className={classes.item}>
+                    <div className={classes.item_img_wrapper}>
+                        <img src={img} alt="cart item" />
+                    </div>
+                    <div className={classes.item_text_wrapper}>
+                        <h3>2 ball of crochet wools</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt et dolore magna aliqua....</p>
+                    </div>
+                    <div className={classes.action_area}>
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div className={classes.empty_cart_notice}>
             <p>Your Cart is Empty, select products to checkout.</p>
