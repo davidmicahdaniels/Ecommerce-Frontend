bdd758ede4ad696965c02e9d04163a0367708b44
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-05-31 17:58:09 +0100

    Working on vendor's dashboard

---
+++ src/Data/VendorsData.js
@@ -0,0 +1,13 @@
+export const VendorsData = [
+    {
+        "vendor_name": "Premium Class Store",
+        "vendor_img": "",
+        "vendor_category": "Accessories store",
+        "address": "Plot 7 Jakande street, afebabalola avenue, Abuja",
+        "city_state": "Abuja, Nigeria",
+        "vendor_description": {
+            "p1": "",
+            "p2": ""
+        }
+    }
+]
