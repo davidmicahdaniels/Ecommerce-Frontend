989ffec35bb08c588dd1f401b49382886c3efe09
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2025-06-01 13:59:34 +0100

    Worning on admin dashboard

--- src/AdminDashboard/ManageVendors/ManageVendors.js
+++ src/AdminDashboard/ManageVendors/ManageVendors.js
@@ -1,14 +1,35 @@
 import React from 'react'
 import classes from "./ManageVendors.module.css"
 import AdminSidebar from '../AdminSidebar/AdminSidebar';
+import img from "../../Assets/Images/prod3.png"
+import { VendorsData } from '../../Data/VendorsData';
 
 const ManageVendors = () => {
   return (
-    <div>
+    <div className={classes.dashboard}>
       <AdminSidebar />
 
       <div className={classes.vendor_display_area}>
+        <h2>Manage Vendors</h2>
 
+        <div className={classes.vendor_cards_wrapper}>
+            {
+                VendorsData.map((vendor) => {
+                    return (
+                        <div className={classes.vendor_card}>
+                                <div className={classes.vendor_img_Wrapper}>
+                                    <img className={classes.vendor_img} src={img} alt='' />
+                                </div>
+                                <div className={classes.vendor_details_Area}>
+                                    <h3>{VendorsData[0].vendor_name}</h3>
+                                    <p>{VendorsData[0].vendor_description.p1}</p>
+                                    <button className={classes.vendor_btn}>Manage Vendor</button>
+                                </div>
+                            </div>
+                        )
+                })
+            }
+        </div>
       </div>
     </div>
   )
